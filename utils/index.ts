6b9da78a575dd0bed28d445ba81d87ceb691eb85
Dani <dani.tasheva@yahoo.com>
2024-02-22 16:46:43 -0700

    Change prod url

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 export const getDomain = () => {
   return new URL(
     process.env.NODE_ENV === "production"
-      ? "https://localhost:3000/coffee-stores-discover.vercel.app"
+      ? "https://coffee-stores-discover.vercel.app"
       : "http://localhost:3000"
   );
 };
